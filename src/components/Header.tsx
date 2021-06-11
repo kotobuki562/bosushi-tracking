f53e8695df1d40c8ef3f24ef0c7010cbe6b3c668
MaQueen <67810971+kotobuki562@users.noreply.github.com>
2021-06-11 19:49:18 +0900

    ⚡️

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,19 +8,23 @@ import { memo } from 'react';
 export const Header = memo(() => {
   const { user } = useAuth0();
   return (
-    <header className="flex fixed z-10 justify-between items-center w-full h-14 text-teal-100 bg-teal-400">
+    <header className="flex fixed z-10 justify-between items-center w-full h-14 text-white bg-teal-400">
       <div className="flex items-center w-14 md:w-64">
-        <div className="flex items-center">
-          <Avatar
-            className="w-14"
-            size="md"
-            name={user?.nickname}
-            src={user?.picture}
-          />
-          <p className="hidden md:block ml-2 text-xl font-semibold text-white">
-            {user?.nickname}
-          </p>
-        </div>
+        <img className="w-14 h-14" src="/img/logo192.png" alt="logo" />
+        <p className="hidden md:block ml-2 text-lg font-semibold">
+          Bosushi Tracking
+        </p>
+      </div>
+      <div className="flex items-center">
+        <p className="hidden md:block mr-2 text-xl font-semibold text-white">
+          {user?.nickname}
+        </p>
+        <Avatar
+          className="w-14"
+          size="md"
+          name={user?.nickname}
+          src={user?.picture}
+        />
       </div>
     </header>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -23,13 +23,13 @@ const links = [
 
 export const Sidebar = () => {
   return (
-    <nav className="group flex fixed top-14 left-0 flex-col items-center w-14 hover:w-64 md:w-64 h-full text-white duration-300 bg-teal-500">
+    <nav className="group flex fixed top-14 left-0 flex-col items-center w-14 hover:w-64 md:w-64 h-full text-white duration-300 bg-teal-800">
       <div className="w-full">
         {links.map((link) => {
           return (
             <Link
               key={link.href}
-              className="flex items-center py-3 pl-3 w-full text-white hover:border-l-4 border-red-100 duration-200 hover:bg-teal-400"
+              className="flex items-center py-3 pl-3 w-full text-white hover:border-l-4 border-red-100 duration-200 hover:bg-teal-700"
               to={link.href}>
               {link.icon}
               <p className="hidden group-hover:block md:block ml-2 font-semibold">
