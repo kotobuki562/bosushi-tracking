ac0e247bcda48686dca865e06f0ca351c90e218b
MaQueen <67810971+kotobuki562@users.noreply.github.com>
2021-06-12 14:58:41 +0900

    ⚡️

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
+/* eslint-disable react/jsx-handler-names */
 /* eslint-disable tailwindcss/no-custom-classname */
 // import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Avatar, Link } from '@chakra-ui/react';
+import { Menu } from '@headlessui/react';
 import { LoginIcon, LogoutIcon } from '@heroicons/react/outline';
 import { memo, useCallback } from 'react';
 
-import { Button } from './Button/Button';
+import { MenuItem } from '../components/Menu/Menu';
 
 // eslint-disable-next-line react/display-name
 export const Header = memo(() => {
@@ -18,8 +20,25 @@ export const Header = memo(() => {
   const handleLoginClick = useCallback(() => {
     return loginWithRedirect();
   }, [loginWithRedirect]);
+
+  const isLoginMenus = [
+    {
+      name: 'LOGOUT',
+      onClick: handleLoagoutClick,
+      icon: <LogoutIcon className="w-5 h-5" />,
+    },
+  ];
+
+  const isLogoutMenus = [
+    {
+      name: 'LOGIN',
+      onClick: handleLoginClick,
+      icon: <LoginIcon className="w-5 h-5" />,
+    },
+  ];
+
   return (
-    <header className="flex fixed z-10 justify-between items-center w-full h-14 text-white bg-teal-200">
+    <header className="flex fixed z-10 justify-between items-center w-full h-14 text-teal-200 bg-teal-800">
       <div className="flex items-center w-14 md:w-64">
         <Link to="/">
           <img className="w-14 h-14" src="/img/logo.png" alt="logo" />
@@ -29,37 +48,71 @@ export const Header = memo(() => {
           Bosushi Tracking
         </p>
       </div>
-      {isAuthenticated ? (
-        <div>
-          <Button
-            useage="delete"
-            btnText="LOGOUT"
-            onClick={handleLoagoutClick}
-            size="md"
-            rightIcon={<LogoutIcon className="w-5 h-5" />}
-          />
-        </div>
-      ) : (
-        <div>
-          <Button
-            useage="other"
-            btnText="LOGIN"
-            onClick={handleLoginClick}
-            size="md"
-            rightIcon={<LoginIcon className="w-5 h-5" />}
-          />
-        </div>
-      )}
       <div className="flex items-center">
-        <p className="hidden md:block mr-2 text-xl font-semibold text-white">
+        <p className="hidden md:block mr-2 text-xl font-semibold">
           {user?.nickname}
         </p>
-        <Avatar
-          className="w-14"
-          size="md"
-          name={user?.nickname}
-          src={user?.picture}
-        />
+        <div className="h-14">
+          {isAuthenticated ? (
+            <MenuItem
+              menuBtn={
+                <Menu.Button className="focus:outline-none">
+                  <Avatar
+                    className="w-14"
+                    size="md"
+                    name={user?.nickname}
+                    src={user?.picture}
+                  />
+                </Menu.Button>
+              }
+              menuItems={
+                <>
+                  {isLoginMenus.map((menu) => {
+                    return (
+                      <Menu.Item key={menu.name}>
+                        <button
+                          onClick={menu.onClick}
+                          className="group flex items-center py-2 px-2 my-1 w-full text-sm rounded-md focus:outline-none text-teal-600 hover:bg-teal-200 ">
+                          {menu.icon}
+                          {menu.name}
+                        </button>
+                      </Menu.Item>
+                    );
+                  })}
+                </>
+              }
+            />
+          ) : (
+            <MenuItem
+              menuBtn={
+                <Menu.Button className="focus:outline-none">
+                  <Avatar
+                    className="w-14"
+                    size="md"
+                    name={user?.nickname}
+                    src={user?.picture}
+                  />
+                </Menu.Button>
+              }
+              menuItems={
+                <>
+                  {isLogoutMenus.map((menu) => {
+                    return (
+                      <Menu.Item key={menu.name}>
+                        <button
+                          onClick={menu.onClick}
+                          className="group flex items-center py-2 px-2 my-1 w-full text-sm rounded-md focus:outline-none text-teal-600 hover:bg-teal-200 ">
+                          {menu.icon}
+                          {menu.name}
+                        </button>
+                      </Menu.Item>
+                    );
+                  })}
+                </>
+              }
+            />
+          )}
+        </div>
       </div>
     </header>
   );
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable tailwindcss/no-custom-classname */
+/* eslint-disable react/display-name */
+import { Menu, Transition } from '@headlessui/react';
+import type { ReactNode, VFC } from 'react';
+import { Fragment, memo } from 'react';
+
+type Props = {
+  menuBtn: ReactNode;
+  menuItems: ReactNode;
+};
+export const MenuItem: VFC<Props> = memo((props) => {
+  return (
+    <div className="w-full text-right">
+      <Menu as="div" className="inline-block relative text-left">
+        <div>{props.menuBtn}</div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95">
+          <Menu.Items className="absolute right-0 w-56 rounded-md divide-y divide-gray-100 ring-1 ring-black ring-opacity-5 shadow-lg origin-top-right focus:outline-none bg-teal-100">
+            <div className="py-1 px-1 ">{props.menuItems}</div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  );
+});
