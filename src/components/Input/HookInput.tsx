68097b9f44d655749690c69f55e41468d246525b
MaQueen <67810971+kotobuki562@users.noreply.github.com>
2021-06-19 17:26:41 +0900

    ⚡️

--- src/components/Input/HookInput.tsx
+++ src/components/Input/HookInput.tsx
@@ -1,38 +1,27 @@
 /* eslint-disable tailwindcss/no-custom-classname */
 /* eslint-disable react/display-name */
-import type { ChangeEventHandler, VFC } from 'react';
+import type { VFC } from 'react';
 import { memo } from 'react';
-import type { FieldValues, UseFormRegister } from 'react-hook-form';
+import type { UseFormRegisterReturn } from 'react-hook-form';
 
 type Props = {
   label: string;
-  name: string;
-  type: string;
-  value: string | number | undefined;
-  onChange: ChangeEventHandler;
   placeholder?: string;
-  hookInputInfo: {
-    register: UseFormRegister<FieldValues>;
-    required?: boolean;
-    pattern?: string;
-    // RegExp?
-    maxLength?: number;
-  };
+
+  register: UseFormRegisterReturn;
+  required?: boolean;
+  pattern?: string;
+  // RegExp?
+  maxLength?: number;
 };
 
-export const Input: VFC<Props> = memo((props) => {
+export const HookInput: VFC<Props> = memo((props) => {
   return (
     <label className="flex flex-col mb-4">
       <span className="mb-2 text-base font-semibold text-teal-600">
         {props.label}
       </span>
       <input
-        {...(props.hookInputInfo.register,
-        {
-          required: props.hookInputInfo.required,
-          maxLength: props.hookInputInfo.maxLength,
-          pattern: props.hookInputInfo.pattern,
-        })}
         className="py-2 px-3 w-full text-lg text-teal-600 bg-opacity-0 rounded-xl border-2 border-gray-200 focus:border-teal-300 shadow-none duration-200 outline-none"
         {...props}
       />
--- src/pages/Items/index.tsx
+++ src/pages/Items/index.tsx
@@ -1,16 +1,191 @@
-/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/naming-convention */
-/* eslint-disable jsx-a11y/iframe-has-title */
-/* eslint-disable react/display-name */
+/* eslint-disable no-console */
 import { gql, useMutation, useQuery } from '@apollo/client';
 import { useAuth0 } from '@auth0/auth0-react';
 import { formatISO } from 'date-fns';
 import type { ChangeEvent } from 'react';
 import { memo, useCallback, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 import { Button } from '../../components/Button/Button';
+import { HookInput } from '../../components/Input/HookInput';
 import { Input } from '../../components/Input/Input';
 import { Layout } from '../../components/Layout';
+
+export const Items = memo(() => {
+  const { register, handleSubmit } = useForm();
+  const { user } = useAuth0();
+  const [addText, setAddText] = useState('');
+  const [itemName, setItemName] = useState('');
+  const [description, setDescription] = useState('');
+  const [slipNum, setSlipNum] = useState<number | undefined>();
+  const [isLoading, setIsLoading] = useState(false);
+  const { data: orderData } = useQuery(GET_ALL_ORDERS);
+  const { loading, error, data: texts } = useQuery(GET_TEXT);
+
+  const [addPostsText] = useMutation(ADD_TEXT, {
+    onCompleted: () => {
+      return setAddText('');
+    },
+  });
+
+  const [addOrders] = useMutation(ADD_ORDERS, {
+    onCompleted: () => {
+      return setAddText('');
+    },
+  });
+
+  const handleSlipNumChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const value = Number(e.target.value);
+      return setSlipNum(value);
+    },
+    []
+  );
+
+  const handleChangeItemName = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      return setItemName(e.target.value);
+    },
+    []
+  );
+
+  const handleChangeDescription = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      return setDescription(e.target.value);
+    },
+    []
+  );
+
+  const onSubmit = (data: any) => {
+    return alert(JSON.stringify(data));
+  };
+
+  const handleAddTodo = async (e: React.FormEvent<HTMLFormElement>) => {
+    setIsLoading(true);
+    e.preventDefault();
+    if (!addText.trim()) return setIsLoading(false);
+    await addPostsText({
+      variables: { text: addText },
+      refetchQueries: [{ query: GET_TEXT }],
+    })
+      .then(() => {
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  };
+
+  const handleAddOrders = async (e: React.FormEvent<HTMLFormElement>) => {
+    setIsLoading(true);
+    e.preventDefault();
+    const createDate = formatISO(new Date());
+    await addOrders({
+      variables: {
+        user_id: user?.sub,
+        createdAt: createDate,
+        updateAt: createDate,
+        delivered: false,
+        itemName: itemName,
+        description: description,
+        number: slipNum,
+      },
+      refetchQueries: [{ query: GET_ALL_ORDERS }],
+    })
+      .then(() => {
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{`Error! ${error.message}`}</p>;
+
+  return (
+    <Layout>
+      <div className="p-8">
+        <h2>テキスト</h2>
+        {texts.posts.map((text: any) => {
+          return <p key={text.id}>{text.text}</p>;
+        })}
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <HookInput
+            register={{ ...register('text') }}
+            maxLength={10}
+            placeholder="テキストを入力"
+            label="テキストを入力"
+            required={true}
+          />
+          <HookInput
+            register={{ ...register('description') }}
+            maxLength={10}
+            placeholder="詳細文"
+            label="詳細文"
+            required={true}
+          />
+          <Button
+            isLoading={isLoading}
+            btnText="送信"
+            type="submit"
+            size="md"
+            useage="other"
+          />
+        </form>
+
+        <h2>オーダーズ</h2>
+        {orderData.orders?.map((order: any) => {
+          return (
+            <div key={order.id}>
+              <p>{order.itemName}</p>
+              <p>{order.description}</p>
+            </div>
+          );
+        })}
+
+        <form onSubmit={handleAddOrders}>
+          <Input
+            label="伝票番号を入力してください。"
+            name="slipNum"
+            value={slipNum}
+            onChange={handleSlipNumChange}
+            placeholder="12桁の伝票番号"
+            type="number"
+          />
+          <Input
+            label="注文品の名前"
+            name="itemName"
+            value={itemName}
+            onChange={handleChangeItemName}
+            placeholder="例）レモン"
+            type="text"
+          />
+          <Input
+            label="詳細文"
+            name="description"
+            value={description}
+            onChange={handleChangeDescription}
+            placeholder="例）アメリカからの輸入用品"
+            type="text"
+          />
+          <Button
+            isLoading={isLoading}
+            btnText="送信"
+            type="submit"
+            size="md"
+            useage="other"
+          />
+        </form>
+      </div>
+    </Layout>
+  );
+});
 
 const GET_TEXT = gql`
   query GetDogs {
@@ -73,171 +248,3 @@ const ADD_ORDERS = gql`
     }
   }
 `;
-
-export const Items = memo(() => {
-  const { user } = useAuth0();
-  const [addText, setAddText] = useState('');
-  const [itemName, setItemName] = useState('');
-  const [description, setDescription] = useState('');
-  const [slipNum, setSlipNum] = useState<number | undefined>();
-  const [isLoading, setIsLoading] = useState(false);
-  const { data: orderData } = useQuery(GET_ALL_ORDERS);
-  const { loading, error, data: texts } = useQuery(GET_TEXT);
-  const [addPostsText] = useMutation(ADD_TEXT, {
-    onCompleted: () => {
-      return setAddText('');
-    },
-  });
-  const [addOrders] = useMutation(ADD_ORDERS, {
-    onCompleted: () => {
-      return setAddText('');
-    },
-  });
-
-  const handleChangeAddText = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      return setAddText(e.target.value);
-    },
-    []
-  );
-
-  const handleSlipNumChange = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      const value = Number(e.target.value);
-      return setSlipNum(value);
-    },
-    []
-  );
-
-  const handleChangeItemName = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      return setItemName(e.target.value);
-    },
-    []
-  );
-
-  const handleChangeDescription = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      return setDescription(e.target.value);
-    },
-    []
-  );
-
-  const handleAddTodo = async (e: React.FormEvent<HTMLFormElement>) => {
-    setIsLoading(true);
-    e.preventDefault();
-    if (!addText.trim()) return setIsLoading(false);
-    await addPostsText({
-      variables: { text: addText },
-      refetchQueries: [{ query: GET_TEXT }],
-    })
-      .then(() => {
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setIsLoading(false);
-      });
-  };
-
-  const handleAddOrders = async (e: React.FormEvent<HTMLFormElement>) => {
-    setIsLoading(true);
-    e.preventDefault();
-    const createDate = formatISO(new Date());
-    await addOrders({
-      variables: {
-        user_id: user?.sub,
-        createdAt: createDate,
-        updateAt: createDate,
-        delivered: false,
-        itemName: itemName,
-        description: description,
-        number: slipNum,
-      },
-      refetchQueries: [{ query: GET_ALL_ORDERS }],
-    })
-      .then(() => {
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setIsLoading(false);
-      });
-  };
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>{`Error! ${error.message}`}</p>;
-
-  return (
-    <Layout>
-      <div className="p-8">
-        <h2>テキスト</h2>
-        {texts.posts.map((text: any) => {
-          return <p key={text.id}>{text.text}</p>;
-        })}
-
-        <form onSubmit={handleAddTodo}>
-          <Input
-            label="テキストを入力"
-            name="text"
-            value={addText}
-            onChange={handleChangeAddText}
-            placeholder="テキストを入力"
-            type="text"
-          />
-          <Button
-            isLoading={isLoading}
-            btnText="送信"
-            type="submit"
-            size="md"
-            useage="other"
-          />
-        </form>
-
-        <h2>オーダーズ</h2>
-        {orderData.orders?.map((order: any) => {
-          return (
-            <div key={order.id}>
-              <p>{order.itemName}</p>
-              <p>{order.description}</p>
-            </div>
-          );
-        })}
-
-        <form onSubmit={handleAddOrders}>
-          <Input
-            label="伝票番号を入力してください。"
-            name="slipNum"
-            value={slipNum}
-            onChange={handleSlipNumChange}
-            placeholder="12桁の伝票番号"
-            type="number"
-          />
-          <Input
-            label="注文品の名前"
-            name="itemName"
-            value={itemName}
-            onChange={handleChangeItemName}
-            placeholder="例）レモン"
-            type="text"
-          />
-          <Input
-            label="詳細文"
-            name="description"
-            value={description}
-            onChange={handleChangeDescription}
-            placeholder="例）アメリカからの輸入用品"
-            type="text"
-          />
-          <Button
-            isLoading={isLoading}
-            btnText="送信"
-            type="submit"
-            size="md"
-            useage="other"
-          />
-        </form>
-      </div>
-    </Layout>
-  );
-});
